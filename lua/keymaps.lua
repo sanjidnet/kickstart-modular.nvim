d58f1f00dee0db25da99779c20a61917e1b305c2
sanjidnet <contact@sanjid.net>
2024-05-09 19:22:02 +1200

    add relative number, jk for normal mode and uncomment some plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,8 @@
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set({ 'i', 'v' }, 'jk', '<Esc>')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
